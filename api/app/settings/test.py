95f6dd5d46399a0daac2b80babd669d07b669031
Gagan Trivedi <gagandeeptrivedi47@gmail.com>
2024-06-14 10:24:38 +0530

    bump default throttle rate

--- api/app/settings/test.py
+++ api/app/settings/test.py
@@ -6,7 +6,7 @@ ENABLE_TELEMETRY = False
 MAX_PROJECTS_IN_FREE_PLAN = 10
 REST_FRAMEWORK["DEFAULT_THROTTLE_CLASSES"] = ["core.throttling.UserRateThrottle"]
 REST_FRAMEWORK["DEFAULT_THROTTLE_RATES"] = {
-    "login": "100/min",
+    "login": "200/min",
     "mfa_code": "5/min",
     "invite": "10/min",
     "signup": "100/min",
